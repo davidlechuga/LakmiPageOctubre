84e8aa09b1d978bbf18173e589a979cf33af83f6
Angel Barrera <47359209+Angel-Barrera@users.noreply.github.com>
2019-12-24 17:51:40 -0600

    se agregó efecto smooth scroll en index

--- js/navegation-page.js
+++ js/navegation-page.js
@@ -1,39 +1,67 @@
 if(innerWidth > 1023) {
-    let navItems = document.querySelector('.nav-content_list'),
+    const navItems = document.querySelector('.nav-content_list'),
         progressBar = document.querySelector('.progress-view'),
-        porcentNumber = document.querySelector('.percentage-number')
-    if (!(navItems && progressBar && porcentNumber))
+        porcentNumber = document.querySelector('.percentage-number'),
+        mainElement = document.querySelector('main')
+
+    if (!(navItems && progressBar && porcentNumber && mainElement))
         console.warn('Elementos faltantes en el DOM')
     else {
-        const totalSecs = navItems.childElementCount,
-              navList = [... navItems.children]
-        let nextSec = 0
+        if(navItems.childElementCount !== mainElement.childElementCount - 1)
+            throw "Longitud de elementos en nav lateral y secciones en main no coincide"
 
+        const navListArray = [... navItems.children],
+            secListArray = [... mainElement.getElementsByClassName('row')],
+            totalSecs = navItems.childElementCount
+
+        let indexSV = 0,
+            wheelAccum = 0
+
+        const scrollCheck = (secView, wheelNow) => {
+            if(indexSV < 1 && wheelNow === -1) return
+            if(indexSV > totalSecs - 2 && wheelNow === 1) return
+            
+            if((wheelNow === 1 && Math.floor(secView.getBoundingClientRect().bottom) < innerHeight + 2)
+                || (wheelNow === -1 && Math.floor(secView.getBoundingClientRect().top) > -2)){
+                indexSV += wheelNow
+                scrollExecute(secListArray[indexSV], navListArray[indexSV].firstElementChild)
+            }
+        }
+        const scrollExecute = (secToView, linkToActive) => {
+            smoothScroll.scrollTo(secToView)
+            location.hash = secToView.id
+            navListArray[
+                navListArray.findIndex(el => el.firstElementChild.classList.length > 1)
+            ].firstElementChild.classList.remove('content_link-active')
+            changeDataView(linkToActive)
+        }
         const changeDataView = (element) => {
-            progressBar.style.height = `${Math.round(nextSec/totalSecs * 100)}%`
-            porcentNumber.textContent = Math.floor(nextSec/totalSecs * 100)
+            progressBar.style.height = `${Math.round((indexSV + 1)/totalSecs * 100)}%`
+            porcentNumber.textContent = Math.floor((indexSV + 1)/totalSecs * 100)
             element.classList.add('content_link-active')
         }
         navItems.addEventListener('click', e => {
             if(e.target.getAttribute('href') !== null){
-                if(e.target.getAttribute('href') === location.hash){
-                    e.preventDefault()
-                    return
-                }
-                nextSec = navList.findIndex(el => el.firstElementChild === e.target) + 1
-                navList[
-                    navList.findIndex(el => [... el.firstElementChild.classList].length > 1)
-                ].firstElementChild.classList.remove('content_link-active')
-                changeDataView(e.target)
+                e.preventDefault()
+                if(e.target.getAttribute('href') === location.hash) return
+                indexSV = navListArray.findIndex(el => el.firstElementChild === e.target)
+                scrollExecute(secListArray[indexSV], e.target)
+            }
+        })
+        mainElement.addEventListener('wheel', e => {
+            wheelAccum += e.deltaY
+            if( Math.abs(wheelAccum) > 0.9) {
+                indexSV = secListArray.findIndex(el => el.id === location.hash.slice(1))
+                scrollCheck(secListArray[indexSV], wheelAccum > 0 ? 1 : -1)
+                wheelAccum = 0
             }
         })
 
-        if(location.hash.length === 0){
-            location.hash = navList[0].firstElementChild.getAttribute('href')
-        } else {
-        nextSec = navList.findIndex(el => 
+        if(location.hash.length === 0)
+            location.hash = secListArray[0].id
+        else indexSV = navListArray.findIndex(el => 
             el.firstElementChild.getAttribute('href') === location.hash)
-        }
-        changeDataView(navList[nextSec++].firstElementChild)
+        
+        changeDataView(navListArray[indexSV].firstElementChild)
     }
 }
