c149b4edfdbfd91fee69c3845ac5d4d53a8cf30f
Angel Barrera <angelbg96.28@gmail.com>
2020-02-14 16:27:40 -0600

    implementación de breadcrum

--- js/services-script.js
+++ js/services-script.js
@@ -1,7 +1,8 @@
 const arrElSL = [... document.querySelectorAll('ul.services__list > li > .list-group-item-action')],
-    arrSubList = [... document.querySelectorAll('.sublist-group')]
+    arrSubList = [... document.querySelectorAll('.sublist-group')],
+    breadcrumb = document.getElementById('breadcrumb-change')
 
-if(!(arrSubList.length && arrElSL.length))
+if(!(arrSubList.length && arrElSL.length && breadcrumb))
     throw "inconsistencia entre elementos con sub-listas"
 
 let lastElemAct, linkUncle, lastSubLink, responseActive, arrSubElAct
@@ -9,6 +10,10 @@ const removeClassActive = (e) => {
         e.preventDefault()
         if(isElementAct(arrSubList, e.target.nextElementSibling, 4) > 0)
             lastElemAct[0].classList.remove('show','active')
+        if(e.target.id === 'EntServ-list'){
+            removeSubElmAct()
+            changeDataBreadcrumb(e.target)
+        }
     },
     toggleItemActive = (e) => {
         if(!e.target.getAttribute('href')) return
@@ -19,9 +24,8 @@ const removeClassActive = (e) => {
             lastElemAct[0].classList.remove('active')
             linkUncle.classList.add('active')
         } else if(responseActive === -1) linkUncle.classList.add('active')
-        arrSubElAct = [... arrSubList.map(el => el.querySelector('.list-group-item-action.active'))].filter(el => el !== null)
-        if(arrSubElAct.length > 0)
-            arrSubElAct.forEach(el => el.classList.remove('active'))
+        removeSubElmAct()
+        changeDataBreadcrumb(linkUncle)
     },
     isElementAct = (arrItems, elSubList, threshold) => {
         lastElemAct = arrItems.filter(el => el.classList.length > threshold)
@@ -29,6 +33,14 @@ const removeClassActive = (e) => {
             if(elSubList !== lastElemAct[0]) return 1
         } else return -1
         return 0
+    },
+    removeSubElmAct = () => {
+        arrSubElAct = [... arrSubList.map(el => el.querySelector('.list-group-item-action.active'))].filter(el => el !== null)
+        if(arrSubElAct.length > 0)
+            arrSubElAct.forEach(el => el.classList.remove('active'))
+    },
+    changeDataBreadcrumb = (newElem) => {
+        breadcrumb.innerText = newElem.innerText
     }
 
 arrElSL.forEach(el => el.addEventListener('click', removeClassActive))
